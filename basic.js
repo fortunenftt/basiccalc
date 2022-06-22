cd10f2a376c281a90a5f43fc411a388e4dd31723
Atewologun Adebowale <bowalewaid@gmail.com>
2022-06-23 06:15:01 +0800

    basic calc

---
+++ basic.js
@@ -0,0 +1,14 @@
+var number1 = parseInt (prompt('number1: '))
+var opr = prompt("operator: - / + *");
+var number2 = parseInt (prompt('number2: '))
+
+if (opr === "+") {alert(number1 + number2);
+    console.log (number1 + number2)}
+  else if (opr === '+'){alert(number1 + number2);
+    console.log (number1 + number2)}
+  else if (opr === '-'){alert(number1 - number2);
+    console.log (number1 - number2)}
+  else if (opr === '*'){alert(number1 * number2);
+    console.log (number1 * number2)}
+  else if (opr === '/'){alert(number1 / number2);
+    console.log (number1 / number2)}
